9206d2e1b28f50261a1fc57bd02bc59cd30e338f
ajimon11092 <ajimon.m@cycloides.co.in>
2020-03-02 15:11:57 +0530

    Initial code

---
+++ src/main/java/com/Test/Application.java
@@ -0,0 +1,29 @@
+
+package com.Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Application.
+ */
+@SpringBootConfiguration
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
